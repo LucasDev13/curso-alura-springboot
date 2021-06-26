b62e9aa3d511ae8ed75fd351dcc2b1b9f6e90570
LucasDev13 <lucaspontes140@gmail.com>
2021-06-26 12:03:07 -0300

    Incluido na classe main do spring a anotação @EnableCache para habilitar o cache, incluido a anotação @Cacheable no método lista no controller e modificação do application.properties com anotações do hibernate

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -32,6 +33,7 @@ public class TopicosController {
     private CursoRepository cursoRepository;
 
     @GetMapping
+    @Cacheable(value = "listaDeTopicos")
     public Page<TopicoDto> lista(@RequestParam (required = false) String nomeCurso,
                                  @PageableDefault(sort = "dataCriacao", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao){
         if (nomeCurso == null){
